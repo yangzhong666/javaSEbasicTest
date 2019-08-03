a48007cf4c314183c9c6a81215665c059edff8c6
yz <1783468996@qq.com>
2019-08-03 10:56:35 +0800

    homework

---
+++ Test2.java
@@ -0,0 +1,12 @@
+public class Test2{
+    public static void main(String[] args){
+		int sumResult = getSum(10,50);
+		
+	
+	}
+	public static void getSum(int num1,int num2){
+		int result = num1+num2;
+		return result;
+	}
+	
+}
