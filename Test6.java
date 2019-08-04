00f6c51522685f1a531b058ee5b9b955e7aef9ff
yz <1783468996@qq.com>
2019-08-05 07:45:12 +0800

    homework

---
+++ Test6.java
@@ -0,0 +1,16 @@
+public class Test6{
+    public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+    System.out.print("x");
+    for (int j = 1; j <= 5; j++) {
+    if(i==1 || i==5 || j ==5)
+    System.out.print("x");
+    else {
+     System.out.print(" ");
+  }
+  }
+    System.out.println();
+ }
+
+ }
+ }
