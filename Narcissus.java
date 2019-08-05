0b092ca8ce87995987e3f6c5341c41bc3e1f8a69
yz <1783468996@qq.com>
2019-08-05 19:32:12 +0800

    homework

---
+++ Narcissus.java
@@ -0,0 +1,15 @@
+public class Narcissus{
+	public static void main(String[] args){
+		//水仙花数
+		//个位 i%0  十位i/10%10 百位 i/100
+		for(int i=100;i<1000;i++){
+			int result = (int)Math.pow(i%10,3)+(int)Math.pow(i/10%10,3)+(int)Math.pow(i/100,3);
+			if(i == result){
+				System.out.println(i);
+			}
+			
+		}
+	
+	}
+
+}
