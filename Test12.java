943f5ae22ed23acf20728b013e8ae29264f1a9e6
yz <1783468996@qq.com>
2019-08-05 19:36:11 +0800

    homework

---
+++ Test12.java
@@ -0,0 +1,27 @@
+//将int类型整数48转换为char类型，并输出。
+public class Test
+{
+    public static void main (String[] args){
+	int b = 48;
+	char c = (char)b;
+	System.out.print(c);
+//将字符中转换为int类型
+	
+	 char d = '中';
+       int  e = (int) d; 
+       System.out.println (e);
+//实现函数计算 
+	
+	
+	
+	
+	
+	
+	
+	}
+}
+
+
+ 
+  
+      
